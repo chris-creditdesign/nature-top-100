a792cd76d7588b1d46bdb872026a9f857edfe0f3
Chris Ryan <creditdesign@gmail.com>
2014-10-19 16:31:34 +0100

    Fix bug with sorting discipline array

--- assets/js/createKey.js
+++ assets/js/createKey.js
@@ -1,11 +1,18 @@
 function createKey (data, colour) {
-	data.sort();
+	
+	var sortedData = [];
+
+	for (var i = 0; i < data.length; i++) {
+		sortedData.push(data[i]);
+	}
+
+	sortedData.sort();
 
 	/* Create checkboxes for each discipline */
 	d3.select(".outer-wrapper .choose-option")
 		.append("ul")
 		.selectAll('li')
-	  .data(data)
+	  .data(sortedData)
 		.enter()
 		.append("li")
 		.style("border-left",function (d, i) {
