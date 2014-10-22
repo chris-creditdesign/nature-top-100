9ac3a735ab2ce795a7ca29f270350f983b9fc9da
Chris Ryan <creditdesign@gmail.com>
2014-10-22 19:12:08 +0100

    Refactor for object pattern

--- assets/js/buildLifeCycle.js
+++ assets/js/buildLifeCycle.js
@@ -1,110 +1,91 @@
-function buildLifeCycle (data, index, margin, width, height, colour) {
-
-	var parseDate = d3.time.format("%Y").parse;
-
-	function prepareData (index) {
-		var lifeCycleData = data[index].lifeCycle;
-
-		for (var i = 0; i < lifeCycleData.length; i++) {
-		  lifeCycleData[i].date = parseDate(lifeCycleData[i].year);
-		}  
-
-		return lifeCycleData.sort(compareYear);
+BuildWidget.prototype.prepareLifeCycelData = function () {
+	for (var i = 0; i < this.data.length; i++) {
+		for (var y = 0; y < this.data[i].lifeCycle.length; y++) {
+			this.data[i].lifeCycle[y].date = this.parseDate(this.data[i].lifeCycle[y].year);
+		}
 	}
+};
 
-	var displayArray = prepareData(index);
-
-	var x = d3.time.scale()
-		.range([0, width])
-		.domain(d3.extent(displayArray, function(d) { return d.date; }));
-
-	var y = d3.scale.linear()
-		.range([height, 0])
-		.domain([0,12000]);
-		// .domain(d3.extent(displayArray, function(d) { return d.cites; }));
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.ticks(5)
-		.orient("bottom");
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.ticks(5)
-		.tickSize(-width, 0)
-		.orient("left");
-
-	var line = d3.svg.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.cites); });
-
-	
-	var svg = d3.select(".outer-wrapper .info-box .life-cycle-chart").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
+BuildWidget.prototype.buildLifeCycle = function () {
+	this.lifeCycleSvg = d3.select(this.target + " > .info-box > .life-cycle-chart").append("svg")
+		.attr("width", this.params.lifeCycleWidth + this.params.lifeCycleMargin.left + this.params.lifeCycleMargin.right)
+		.attr("height", this.params.lifeCycleHeight + this.params.lifeCycleMargin.top + this.params.lifeCycleMargin.bottom)
 	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("transform", "translate(" + this.params.lifeCycleMargin.left + "," + this.params.lifeCycleMargin.top + ")");
 
-	svg.append("g")
+	this.lifeCycleSvg.append("g")
 		.attr("class","white-box")
 	  .append("rect")
 		.attr("x", 0)
 		.attr("y", 0)
-		.attr("width", width)
-		.attr("height", height)
+		.attr("width", this.params.lifeCycleWidth)
+		.attr("height", this.params.lifeCycleHeight)
 		.attr("fill","#FFF");
+};
 
-	svg.append("g")
+BuildWidget.prototype.buildLifeCycleScales = function () {
+	this.xScaleLifeCycle = d3.time.scale()
+		.range([0, this.params.lifeCycleWidth])
+		.domain(d3.extent(this.data[this.displayIndex].lifeCycle, function(d) { return d.date; }));
+
+	this.yScaleLifeCycle = d3.scale.linear()
+		.range([this.params.lifeCycleHeight, 0])
+		.domain([0,12000]);
+
+};
+
+BuildWidget.prototype.buildLifeCycleAxes = function () {
+	var self = this;
+
+	this.xAxisLifeCycle = d3.svg.axis()
+		.scale(this.xScaleLifeCycle)
+		.ticks(5)
+		.orient("bottom");
+
+	this.yAxisLifeCycle = d3.svg.axis()
+		.scale(this.yScaleLifeCycle)
+		.ticks(5)
+		.tickSize(-this.params.lifeCycleWidth, 0)
+		.orient("left");
+
+	this.line = d3.svg.line()
+		.x(function(d) { return self.xScaleLifeCycle(d.date); })
+		.y(function(d) { return self.yScaleLifeCycle(d.cites); });
+
+	this.lifeCycleSvg.append("g")
 	    .attr("class", "x axis")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(xAxis);
+	    .attr("transform", "translate(0," + this.params.lifeCycleHeight + ")")
+	    .call(this.xAxisLifeCycle);
 
-	svg.append("g")
+	this.lifeCycleSvg.append("g")
 	    .attr("class", "y axis")
-	    .call(yAxis)
+	    .call(this.yAxisLifeCycle)
 	  .append("text")
-	    .attr("transform", "translate(" + -(margin.left * 0.9) + "," + (height/2) + "), rotate(-90)")
+	    .attr("transform", "translate(" + -(this.params.lifeCycleMargin.left * 0.9) + "," + (this.params.lifeCycleHeight/2) + "), rotate(-90)")
 	    .attr("y", 6)
 	    .attr("dy", ".5em")
 	    .style("text-anchor", "middle")
 	    .text("Citations per year");
 
-	svg.append("path")
-	    .datum(displayArray)
-	    .attr("class", "line")
-	    .attr("d", line);
+	this.lifeCycleSvg.append("path")
+		.datum(this.data[this.displayIndex].lifeCycle)
+		.attr("class", "line")
+		.attr("d", this.line);
+};
 
-	function updateLine(data) {
-	
+// Still needs to be tested!
+BuildWidget.prototype.updateLifeCycleLine = function () {
+	this.xScaleLifeCycle.domain(d3.extent(this.data[this.displayIndex].lifeCycle, function(d) { return d.date; }));
 
-		var displayArray = prepareData(data);
-
-		x.domain(d3.extent(displayArray, function(d) { return d.date; }));
-		// y.domain(d3.extent(displayArray, function(d) { return d.cites; }));
-
-		/* Call the Y axis to adjust it to the new scale */
-		// svg.select(".outer-wrapper .life-cycle-chart .y")
-		// 	.transition()
-		// 	.duration(150)
-		// 	.call(yAxis);
-
-		svg.select(".outer-wrapper .life-cycle-chart .x")
-			.transition()
-			.duration(150)
-			.call(xAxis);
+	/* Can this work better */
+	// Store a reference to the axis and the line in the  BuildWidget object? Innit? 
+	this.lifeCycleSvg.select(".outer-wrapper .life-cycle-chart .x")
+		.transition()
+		.duration(150)
+		.call(xAxis);
 
 
-		svg.selectAll(".outer-wrapper .life-cycle-chart path.line")
-			.data([displayArray])
-			.attr("d", line);
-	}
-
-	return {
-		updateLine: function (index) {
-			updateLine(index);
-		}
-
-	};
-
-
-}
+	this.lifeCycleSvg.selectAll(".outer-wrapper .life-cycle-chart path.line")
+		.data([this.data[this.displayIndex].lifeCycle])
+		.attr("d", this.line);
+};
