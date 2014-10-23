cc597d8615cdfa6640757ffdee5febf9c3dabfe1
Chris Ryan <creditdesign@gmail.com>
2014-10-23 15:46:07 +0100

    Add new colours and move params inside init

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,40 @@
 			/*	Loading D3 into ie6-8 seems to cause a runtime error */
 			$.getScript("http://www.nature.com/polopoly_static/js/d3.v3.min.js", function() {
 				
+				var thRoParams = {};
+
+				thRoParams.colour =  ["#c1272d",
+										"#f15a24",
+										"#fbb03b",
+										"#d9e021",
+										"#39b54a",
+										"#00a99d",
+										"#0071bc",
+										"#1b1464",
+										"#93278f",
+										"#c69c6d",
+										"#8c6239",
+										"#754c24",
+										"#42210b"];
+
+				/*	Margin, Width and height */
+				thRoParams.margin = {top: 40, right: 40, bottom: 40, left: 80, mid: 40};
+				thRoParams.lifeCycleMargin = {top: 10, right: 20, bottom: 20, left: 70};
+				thRoParams.width = jQuery('.section').width()  - thRoParams.margin.left - thRoParams.margin.right;
+				thRoParams.miniHeight = 60;
+				thRoParams.lifeCycleHeight = 100;
+				thRoParams.lifeCycleWidth = jQuery('.info-box').width() - thRoParams.lifeCycleMargin.left - thRoParams.lifeCycleMargin.right;
+				thRoParams.height = 400 - thRoParams.margin.top - thRoParams.margin.mid - thRoParams.miniHeight - thRoParams.margin.bottom;
+				thRoParams.handleWidth = 15;
+				/*	Global variable to control the length of D3 transitons */
+				thRoParams.duration = 450;
+				thRoParams.delay = 450;
+				thRoParams.activeRecord = {};
+				thRoParams.topData = [];
+				thRoParams.displayArray = [];
+				thRoParams.disciplineArray = [];
+				
+				
 				var thRoGraphic = new BuildWidget("#tr-graphic", thRoParams, thRoData);
 
 				extendObject(thRoGraphic.pubsub);
